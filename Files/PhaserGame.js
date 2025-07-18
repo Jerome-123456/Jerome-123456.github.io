cddcc81dfb7eb9690f550c0a581c361176f37b39
Jerome-123456 <DAmbrose25@synergymat.uk>
2025-07-18 13:20:01 +0100

    Add player movement and update assets in Phaser game
    Replaced old background and sprite assets with new images. Updated PhaserGame.js to load new assets, initialize the player sprite, and implement left/right movement using arrow keys. Adjusted game dimensions and updated index.html to reflect changes and provide instructions.

--- Files/PhaserGame.js
+++ Files/PhaserGame.js
@@ -1,24 +1,44 @@
+const gameState = {}
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 576,
+    height: 324,
     scene: {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    parent: 'game',
 };
 
-var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('Background', 'Assets/background 1.png');
+    this.load.spritesheet('Player', 'Assets/player.png', { frameWidth: 50, frameHeight: 50 });
 }
 
 function create ()
 {
+    this.add.image(288, 162, 'Background');
+
+    gameState.player = this.add.sprite(565,300,'Player')
+
+    gameState.cursors = this.input.keyboard.createCursorKeys()
+    
 }
 
 function update ()
 {
+    if(gameState.cursors.right.isDown && gameState.player.x < 576){
+        gameState.player.x += 2;
+    }
+    if(gameState.cursors.left.isDown && gameState.player.x > 0){
+        gameState.player.x -= 2;
+    }
+    
+    
+
 }
+
+const game = new Phaser.Game(config);
